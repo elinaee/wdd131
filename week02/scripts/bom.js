ff6e520fde51af743d8fe0923f3cdeb9060a2aea
elinaee <elissa@live.com.pt>
2024-06-20 07:45:07 +0200

    bom activity - js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -4,20 +4,39 @@ const list = document.querySelector('#list');
 
 
 // The following li element will hold each entries chapter title and an associated delete button.
-const li = document.createElement ('li');
+//const li = document.createElement ('li');
 
-const deleteButton = document.createElementNS('button');
+//const deleteButton = document.createElementNS('button');
 
-li.textContent = input.value;
+//li.textContent = input.value;
 
 
 // add the following icon to the delete button: ❌
-deleteButton.textContent = '❌';
+//deleteButton.textContent = '❌';
 
 
 // append the li element variable  with the delete button.
-li.append(deleteButton);
+//li.append(deleteButton);
 
 
 // append the li element variable to the unordered list in your HTML.
-list.append(list);
+//list.append(li);
+
+// this adds a click event listener for the 'add chapter'button using a addEventListener.
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li)
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+        input.value = '';
+        input.focus();
+    }
+})
+
